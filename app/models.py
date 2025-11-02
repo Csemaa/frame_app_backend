929bfae39f6d6efd68440f081bd0bfc3425a0154
Csemeczki András <Andras Csemeczki>
2025-11-02 14:15:03 +0100

    models corrected

--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ class Movie(db.Model):
     path = db.Column(db.String(512))
     last_time_viewed = db.Column(db.Datetime(), nullable=True, default=datetime.now().isoformat())
 
+    tags = db.relationship('UserTag', back_populates='movie', cascade='all, delete-oprhan')
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key= True)
@@ -15,12 +17,17 @@ class User(db.Model):
     nickname = db.Column(db.String(32))
     profile_picture = db.Column(db.String(32))
 
+    tags = db.relationship('UserTag', back_populates='user', cascade='all, delete-oprhan')
+
 
 class UserTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    movie_id = db.Column(db.Integer)
+    user_id = db.Column(db.ForeignKey('users.id'), nullable=False)
+    movie_id = db.Column(db.ForeignKey('movies.id'), nullable=False)
     tag = db.Column(db.String(128)) # For store watch later, and favourite values
+
+    user = db.relationship('User', back_populates='tags')
+    movie = db.relationship('Movie', back_populates='tags')
 
     
 
